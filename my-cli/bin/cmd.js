12e45c4cf882d431ba2ed854852466e8d6aec472
Luis Rangel <luis.rangel@telus.com>
2023-07-13 19:42:15 -0600

    feat: get version from package.json

--- my-cli/bin/cmd.js
+++ my-cli/bin/cmd.js
@@ -1,12 +1,32 @@
 #!/usr/bin/env node
 import got from "got";
+import { Command } from "commander";
+import { readFile } from 'fs/promises';
+
+const pkg = JSON.parse(await readFile(new URL('../package.json', import.meta.url)));
+
 const API = "http://localhost:3000";
+
 const usage = (msg = "Back office for My App") => {
   console.log(`\n${msg}\n`);
   console.log("Usage: cmd <ID> <AMOUNT>");
 };
+
+// Create a new Command Program
+const program = new Command();
+
+// Create a new Program
+program
+  .name("my-cli") // Set the name of the program
+  .description("Back office for My App") // Set the description
+  .version(pkg.version); // Set the version
+// Parse the arguments from process.argv
+program.parse();
+
+
 // Get the arguments from the command line
 const argv = process.argv.slice(2);
+
 // If there are no arguments, show the usage and exit
 if (argv.length < 2) {
   usage();
@@ -18,11 +38,13 @@ const [argID, argAmount] = argv;
 
 // Check if the Amount is a number
 const amount = parseInt(argAmount);
+
 // If the amount is not a number, show the usage and exit
 if (isNaN(amount)) {
   usage("Error: <AMOUNT> must be a number");
   process.exit(1);
 }
+
 // Update the order with the given ID
 try {
   // Use GOT to make a POST request to the API
